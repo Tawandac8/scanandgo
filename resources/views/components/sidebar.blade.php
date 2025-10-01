f1195fde35b20497755600909665efe9af02f7c6
Tawandac8 <43347294+Tawandac8@users.noreply.github.com>
2025-10-01 10:03:38 +0200

    edited right to access the users

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -113,6 +113,7 @@
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Admin Settings</h6>
         </li>
+        @hasanyrole('admin|super-admin')
         <li class="nav-item">
           <a class="nav-link" href="{{ route('admin.all-users') }}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -154,6 +155,7 @@
             <span class="nav-link-text ms-1">Settings</span>
           </a>
         </li>
+        @endhasanyrole
       </ul>
     </div>
   </aside>
