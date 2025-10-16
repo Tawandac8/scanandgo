1cf239684b9f9a289f849aa036d21da93cf87df6
Tawandac8 <tawandachibayiwa@gmail.com>
2025-10-16 20:10:56 +0200

    m

--- resources/views/reports/report.blade.php
+++ resources/views/reports/report.blade.php
@@ -66,7 +66,7 @@
                   <h6>{{ count($countries) }} Countries</h6>
                 </div>
                 <div class="col-lg-6 col-5 my-auto text-end">
-                  
+
                 </div>
               </div>
             </div>
@@ -95,6 +95,32 @@
           </div>
         </div>
       </div>
+      <div class="row">
+        @foreach($otherBadges as $badgetype)
+  <div class="col-lg-3 col-md-3 col-12 mt-4 mt-md-0">
+              <div class="card">
+                <span class="mask bg-gradient-primary opacity-10 border-radius-lg"></span>
+                <div class="card-body p-3 position-relative">
+                  <div class="row">
+                    <div class="col-8 text-start">
+                      <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
+                        <i class="fa-solid fa-person-walking-luggage"></i>
+                      </div>
+                      <h5 class="text-white font-weight-bolder mb-0 mt-3">
+                        {{ $badgetype->badges->count() }}
+                      </h5>
+                    </div>
+                    <div class="col-4">
+                      <div class="dropstart text-end mb-6">
+                      </div>
+                      <p class="text-white text-sm text-end font-weight-bolder mt-auto mb-0">{{$badgetype->name}}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            @endforeach
+        </div>
 @endsection
 
 @section('scripts')
