bff0f2908eae1bee023f3d1ee70c034e1a3f9b4c
Tawandac8 <43347294+Tawandac8@users.noreply.github.com>
2026-01-19 13:08:41 +0200

    scanandgo

--- routes/console.php
+++ routes/console.php
@@ -27,24 +27,36 @@ Schedule::call(function () {
 
             foreach($badges as $badge){
                 $badge_exists = Badge::where('reg_code',$badge['registration_code'])->first();
+
+                if($badge['profile']){
+                    $company_name = isset($badge['profile']['company'])? $badge['profile']['company']['name'] : $badge['profile']['student']['school_name'];
+                    $position = isset($badge['profile']['company'])? $badge['profile']['company']['department'] : $badge['profile']['student']['level'];
+                    $city = isset($badge['profile']['city'])? $badge['profile']['city']['name'] : '';
+                    $country = isset($badge['profile']['country'])? $badge['profile']['country']['name'] : '';
+                }else{
+                    $company_name = $badge['company'];
+                    $position = $badge['position'];
+                    $city = $badge['city'];
+                    $country = $badge['country'];
+                }
                 
                 if(!$badge_exists){
                         Badge::create([
                             'badge_type' => $badge_type->name,
                             'badge_type_id' => $badge_type->id,
-                            'title' => $badge['profile']['title'],
-                            'first_name' => $badge['profile']['first_name'],
-                            'last_name' => $badge['profile']['last_name'],
-                            'company_name' => isset($badge['profile']['company'])? $badge['profile']['company']['name'] : $badge['profile']['student']['school_name'],
+                            'title' => ($badge['profile'])? $badge['profile']['title'] : $badge['title'],
+                            'first_name' => ($badge['profile'])? $badge['profile']['first_name'] : $badge['first_name'],
+                            'last_name' => ($badge['profile'])? $badge['profile']['last_name'] : $badge['last_name'],
+                            'company_name' => $company_name,
                             'reg_code' => $badge['registration_code'],
                             'event' => $event->name,
                             'is_online_registration' => 1,
-                            'position' => isset($badge['profile']['company'])? $badge['profile']['company']['department'] : $badge['profile']['student']['level'],
+                            'position' => $position,
                             'event_id'=> $event->id,
-                            'mobile' => $badge['profile']['phone'],
-                            'city' => isset($badge['profile']['city'])? $badge['profile']['city']['name'] : '',
-                            'country' => isset($badge['profile']['country'])? $badge['profile']['country']['name']:'',
-                            'email' => $badge['profile']['user'] ? $badge['profile']['user']['email'] : ''
+                            'mobile' => ($badge['profile'])? $badge['profile']['phone'] : $badge['phone'],
+                            'city' => $city,
+                            'country' => $country,
+                            'email' => ($badge['profile'])? $badge['profile']['user']['email'] : $badge['email']
                         ]);
                     }
             }
