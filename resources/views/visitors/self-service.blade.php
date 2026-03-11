c129651bee00c698c34f0a47da8a65f7b631877a
Tawandac8 <43347294+Tawandac8@users.noreply.github.com>
2026-03-11 20:03:41 +0200

    schedule board meeting reminders

--- resources/views/visitors/self-service.blade.php
+++ resources/views/visitors/self-service.blade.php
@@ -7,14 +7,10 @@
     <title>Self-Service Badge Printing</title>
     <!-- Fonts and icons -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="{{asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
-    <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
     <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="pagestyle" href="{{asset('assets/css/soft-ui-dashboard.css?v=1.0.7')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <!-- CSS Files (Bootstrap for layout and spinner) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     
     <style>
         body, html {
@@ -112,7 +108,7 @@
     <!-- Hidden area for printing the badge -->
     <div id="print-area" style="display: none;"></div>
 
-    <script src="{{asset('assets/js/core/jquery.min.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
             const inputField = $('#reg_code');
