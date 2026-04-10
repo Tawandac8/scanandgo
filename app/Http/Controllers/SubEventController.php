7803ffc50a0d319e38ec4ef3e7f3db75f3271d64
Tawandac8 <43347294+Tawandac8@users.noreply.github.com>
2026-04-10 14:25:05 +0200

    serial numbers

--- app/Http/Controllers/SubEventController.php
+++ app/Http/Controllers/SubEventController.php
@@ -153,46 +153,38 @@ class SubEventController extends Controller
         //get delegate badge type
         $delegate_badge = BadgeType::where('name','Delegate')->first();
 
-        $event = SubEvent::where('id',$request->get('event_id'))->first();
-       
+        $event = SubEvent::where('id', $request->get('event_id'))->first();
+
         //search delegates
-        $badges = Badge::where('sub_event_id',$event->id)->where('badge_type_id',$delegate_badge->id)->where('name','like','%'.$request->query.'%')->get();
-        return $badges;
+        $badges = Badge::where('sub_event_id', $event->id)
+            ->where('badge_type_id', $delegate_badge->id)
+            ->where('is_printed', 0)
+            ->where('is_hidden', 0)
+            ->where('name', 'like', '%' . $request->get('query') . '%')
+            ->get();
 
         $output = '';
 
         foreach($badges as $badge){
-
             $output .= '<tr>
                 <td>
                     <div class="d-flex px-2 py-1">
                         <div class="d-flex flex-column justify-content-center">
-                            <h6 class="mb-0 text-sm">'.$badge->name.'}}</h6>
+                            <h6 class="mb-0 text-sm">'.$badge->name.'</h6>
                         </div>
                     </div>
                 </td>
-                    <td>
-                        <span class="text-xs font-weight-bold">'.$badge->company_name.'</span>
-                    </td>
-                    <td>
-                        <span class="text-xs font-weight-bold">'.$badge->badge_type->name.'</span>
-                    </td>
-                    <td>';
-                    if($badge->is_printed){
-                         $output .= '<span class="badge badge-sm bg-gradient-success">Yes</span>';
-                    }else{
-                        $ouput .= '<span class="badge badge-sm bg-gradient-danger">No</span>';
-                    }
-
+                <td>';
+                   if($badge->is_printed){
+                        $output .= '<span class="badge badge-sm bg-gradient-success">Yes</span>';
+                   }else{
+                       $output .= '<span class="badge badge-sm bg-gradient-danger">No</span>';
+                   }
             $output .= '</td>
-                <td class="align-middle text-center text-sm">
-                    <span class="text-xs font-weight-bold"> {{ $badge->printed_copies }} </span>
-                </td>
                 <td>
-                    <span onclick="viewBadge({{ $badge->id }})" style="cursor: pointer" class="badge badge-sm bg-gradient-info">Go <i class="fa-solid fa-arrow-right"></i></span>
+                    <span onclick="viewBadge('.$badge->id.')" style="cursor: pointer" class="badge badge-sm bg-gradient-info">Go <i class="fa-solid fa-arrow-right"></i></span>
                 </td>
             </tr>';
-                        
         }
 
         return $output;
